3a51d694d132995c78bc0d77bcfa8bf7224a5b4a
Vichaoss <viceaperez@gmail.com>
2024-02-24 20:01:08 -0300

    fast one

--- wksp_grnd/acad_amounter.py
+++ wksp_grnd/acad_amounter.py
@@ -30,6 +30,12 @@ class Sector:
         self.conectT4_2 = []
         self.conectT4_4 = []
         self.conectCruz = []
+
+        self.lineas_r = []
+        self.largo_r = 0
+        self.conectT4_2_r = []
+        self.conectT4_4_r = []
+        self.conectCruz_r = []
         pass
 
     pass
@@ -41,7 +47,9 @@ class AcadMan:
     ignored_layers = [
         "OTROS",
         "Chicotes",
-        "0"
+        "0",
+        "Anotaciones",
+        "Defpoints"
     ]
 
     objs = []
@@ -50,29 +58,12 @@ class AcadMan:
     @classmethod
     def go(cls):
         for i in cls.objs:
-            result = Tramo(i.Layer)
+            result = i.Layer
             for j in cls.sectores:
-                if j.tramo != result:
-                    continue
-                j.pool.append(i)
-                if i.EntityName == "AcDbLine":
-                    j.lineas.append(i)
-                    break
-                    pass
-                elif i.EntityName == "AcDbHatch":
-                    if 190100 >= i.Area >= 189900:
-                        j.conectT4_4.append(i)
-                        break
-                        pass
-                    elif 170100 >= i.Area >= 159900:
-                        j.conectT4_2.append(i)
-                        break
-                        pass
-                    elif 240100 >= i.Area >= 239900:
-                        j.conectCruz.append(i)
-                        break
-                        pass
-                    pass
+                if j.tramo.value == result:
+                    cls.go_layer(j, i)
+                elif j.tramo.value + "-listo" == result:
+                    cls.go_listo(j, i)
                 pass
             pass
         cls.consistency()
@@ -109,6 +100,12 @@ class AcadMan:
             temp.conectT4_4 = []
             temp.conectT4_2 = []
             temp.conectCruz = []
+
+            temp.lineas_r = []
+            temp.largo_r = 0
+            temp.conectT4_2_r = []
+            temp.conectT4_4_r = []
+            temp.conectCruz_r = []
             cls.sectores.append(temp)
             pass
         cls.fetch()
@@ -117,7 +114,7 @@ class AcadMan:
     @classmethod
     def consistency(cls):
         for i in cls.sectores:
-            if len(i.lineas)+len(i.conectT4_2)+len(i.conectT4_4)+len(i.conectCruz) == len(i.pool):
+            if len(i.lineas) + len(i.conectT4_2) + len(i.conectT4_4) + len(i.conectCruz) == len(i.pool):
                 i.consistencia = True
                 pass
             else:
@@ -126,10 +123,84 @@ class AcadMan:
             for j in i.lineas:
                 i.largo += j.Length
                 pass
-            i.largo = i.largo/1000
+            for j in i.lineas_r:
+                i.largo_r += j.Length
+                pass
+            i.largo = i.largo / 1000
+            i.largo_r = i.largo_r / 1000
+        pass
+
+    @classmethod
+    def go_listo(cls, j, i):
+        j.pool.append(i)
+        if cls.is_line(i):
+            j.lineas_r.append(i)
+            return
+        elif cls.is_conectT4_4(i):
+            j.conectT4_4_r.append(i)
+            return
+        elif cls.is_conectT4_2(i):
+            j.conectT4_2_r.append(i)
+            return
+        elif cls.is_conectCruz(i):
+            j.conectCruz_r.append(i)
+            return
+        pass
+
+    @classmethod
+    def is_line(cls, i):
+        return i.EntityName == "AcDbLine"
+
+    @classmethod
+    def is_conectT4_4(cls, i):
+        return i.EntityName == "AcDbHatch" and 190100 >= i.Area >= 189900
+
+    @classmethod
+    def is_conectT4_2(cls, i):
+        return i.EntityName == "AcDbHatch" and 170100 >= i.Area >= 159900
+
+    @classmethod
+    def is_conectCruz(cls, i):
+        return i.EntityName == "AcDbHatch" and 240100 >= i.Area >= 239900
+
+    @classmethod
+    def go_layer(cls, j, i):
+        j.pool.append(i)
+        if cls.is_line(i):
+            j.lineas.append(i)
+            return
+        elif cls.is_conectT4_4(i):
+            j.conectT4_4.append(i)
+            return
+        elif cls.is_conectT4_2(i):
+            j.conectT4_2.append(i)
+            return
+        elif cls.is_conectCruz(i):
+            j.conectCruz.append(i)
+            return
+        pass
+
+    @classmethod
+    def out(cls):
+        fl = open("out.txt", "w")
+        fl.write(
+            "Sector\tlargo\tconectT4_2\tconectT4_4\tconectCruz\tlargo listo\tconectT4_2 listo\tconectT4_4 listo\tconectCruz listo\n")
+        for i in cls.sectores:
+            fl.write(i.tramo.value + "\t")
+            fl.write(str(i.largo).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectT4_2)).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectT4_4)).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectCruz)).replace(".", ",") + "\t")
+            fl.write(str(i.largo_r).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectT4_2_r)).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectT4_4_r)).replace(".", ",") + "\t")
+            fl.write(str(len(i.conectCruz_r)).replace(".", ",") + "\n")
+            pass
+        fl.close()
         pass
 
 
 AcadMan.init()
 AcadMan.go()
+AcadMan.out()
 pass
